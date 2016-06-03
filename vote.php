d8e8dc97492cef57856097e9b03c0f094d88b832
Jakub Ziemann <ziemannjakub@gmail.com>
2016-06-03 18:56:10 +0200

    Connection with cba.pl

--- vote.php
+++ vote.php
@@ -1,8 +1,8 @@
 <?php
-$user = 'root';
-$password = 'root';
-$dbname = 'invitro';
-$host = 'localhost';
+$user = 'kasiakuba';
+$password = 'invitro';
+$dbname = 'in_vitro_pz_cba_pl';
+$host = 'mysql.cba.pl';
 
 $conn = new mysqli($host, $user, $password, $dbname);
 
@@ -10,23 +10,20 @@ if($conn->connect_error) {
    die("Connection failed: " .$conn->connect_error);
 }
 
+$a1 = $_POST['popiera'];
+$a2 = $_POST['zajscie'];
+$a3 = $_POST['zycie'];
+$a4 = $_POST['wiara'];
+$a5 = $_POST['plec'];
+$a6 = $_POST['wiek'];
+$a7 = $_POST['zamieszkanie'];
+$a8 = $_POST['wyksztalcenie'];
+$a9 = $_POST['dzieci'];
+$a10 = $_POST['zwiazek'];
 
 
 
-   $a1 = $_POST['popiera'];
-   $a2 = $_POST['zajscie'];
-   $a3 = $_POST['zycie'];
-   $a4 = $_POST['wiara'];
-   $a5 = $_POST['plec'];
-   $a6 = $_POST['wiek'];
-   $a7 = $_POST['zamieszkanie'];
-   $a8 = $_POST['wyksztalcenie'];
-   $a9 = $_POST['dzieci'];
-   $a10 = $_POST['zwiazek'];
-
-
-
-   $sql = "INSERT INTO ankieta (pytanie1,pytanie2,pytanie3,pytanie4,pytanie5,pytanie6,pytanie7,pytanie8,pytanie9,pytanie10) VALUES ('$a1','$a2','$a3','$a4','$a5','$a6','$a7','$a8','$a9','$a10')";
+$sql = "INSERT INTO ankieta (pytanie1,pytanie2,pytanie3,pytanie4,pytanie5,pytanie6,pytanie7,pytanie8,pytanie9,pytanie10) VALUES ('$a1','$a2','$a3','$a4','$a5','$a6','$a7','$a8','$a9','$a10')";
 
 
 
@@ -34,31 +31,27 @@ if($conn->connect_error) {
 
 
 
-   if($conn->query($sql) === TRUE) {
-      echo "<html lang=\"en\">
-            <head>
-               <meta charset=\"utf-8\">
-               <title>Ankieta na temat in vitro</title>
+if($conn->query($sql) === TRUE) {
+   echo "<html lang=\"en\">
+   <head>
+   <meta charset=\"utf-8\">
+   <title>Ankieta na temat in vitro</title>
 
-               <link rel=\"stylesheet\" href=\"css/main.css\" />
-            </head>
-            <body>
-               <div class = title>
-               <p>
-                  Dziękujemy za wypełnienie ankiety
-               </p>
-               </div>
-            </body>
-      </html>";
+   <link rel=\"stylesheet\" href=\"css/main.css\" />
+   </head>
+   <body>
+   <div class = title>
+   <p>
+   Dziękujemy za wypełnienie ankiety
+   </p>
+   </div>
+   </body>
+   </html>";
 
-   } else {
-      echo "Error: " .$sql . "<br>" . $conn->error;
-   }
-
-
-
-
+} else {
+   echo "Error: " .$sql . "<br>" . $conn->error;
+}
 
 $conn->close();
 
- ?>
+?>
